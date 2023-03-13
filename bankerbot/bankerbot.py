f41814bd1f86be4ad58bcc2444f96ba996d1d217
Tyler J <tylerj714@gmail.com>
2023-03-13 09:11:06 -0500

    Limit player choices to only those registered to the game anda live working

--- bankerbot/bankerbot.py
+++ bankerbot/bankerbot.py
@@ -2,7 +2,7 @@
 import os
 import discord
 from dotenv import load_dotenv
-from discord import app_commands
+from discord import app_commands, Member, Guild
 from typing import List, Optional, Literal
 import banker_dom
 from banker_dom import Game, Player, Faction, Round, Vote
@@ -12,6 +12,8 @@ import time
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD_ID = int(os.getenv('GUILD_ID'))
+PLAYER_ROLE_ID = int(os.getenv('PLAYER_ROLE_ID'))
 BASE_PATH = os.getenv('BASE_PATH')
 
 game_factions = Literal["Van der Linde Gang",
@@ -35,7 +37,7 @@ class BankerBotClient(discord.Client):
     async def on_ready(self):
         await self.wait_until_ready()
         if not self.synced:
-            await tree.sync(guild=discord.Object(id=947928220093788180))
+            await tree.sync(guild=discord.Object(id=GUILD_ID))
             self.synced = True
         print(f"We have logged in as {self.user}.")
 
@@ -43,9 +45,26 @@ class BankerBotClient(discord.Client):
 client = BankerBotClient()
 tree = app_commands.CommandTree(client)
 
+async def player_list_autocomplete(interaction: discord.Interaction,
+                                   current: str,
+                             ) -> List[app_commands.Choice[str]]:
+    game = await get_game(BASE_PATH)
+    players = await get_valid_players(game.players)
+    return [
+        app_commands.Choice(name=player.player_discord_name, value=str(player.player_id))
+        for player in players
+    ]
+
+async def get_valid_players(players: List[Player]) -> List[Player]:
+    player_list = []
+    for player in sorted(players, key=lambda e: e.player_discord_name.lower()):
+        if not player.is_dead:
+            player_list.append(player)
+    return player_list
+
 @tree.command(name="toggle-activity",
               description="Enables/Disables bot commands for players",
-              guild=discord.Object(id=947928220093788180))
+              guild=discord.Object(id=GUILD_ID))
 @app_commands.default_permissions(manage_guild=True)
 async def toggle_activity(interaction: discord.Interaction,
                           active: Literal['True', 'False']):
@@ -60,7 +79,7 @@ async def toggle_activity(interaction: discord.Interaction,
 
 @tree.command(name="post-embed",
               description="Posts a pre-computed embed to a channel",
-              guild=discord.Object(id=947928220093788180))
+              guild=discord.Object(id=GUILD_ID))
 @app_commands.default_permissions(manage_guild=True)
 @app_commands.choices(embed=embed_choices)
 async def post_embed(interaction: discord.Interaction,
@@ -77,7 +96,7 @@ async def post_embed(interaction: discord.Interaction,
 
 @tree.command(name="add-faction",
               description="Adds a faction to the game",
-              guild=discord.Object(id=947928220093788180))
+              guild=discord.Object(id=GUILD_ID))
 @app_commands.default_permissions(manage_guild=True)
 async def add_faction(interaction: discord.Interaction,
                       faction: game_factions,
@@ -102,7 +121,7 @@ async def add_faction(interaction: discord.Interaction,
 
 @tree.command(name="add-player",
               description="Adds a player to the game",
-              guild=discord.Object(id=947928220093788180))
+              guild=discord.Object(id=GUILD_ID))
 @app_commands.default_permissions(manage_guild=True)
 async def add_player(interaction: discord.Interaction,
                      player: discord.Member,
@@ -146,7 +165,7 @@ async def add_player(interaction: discord.Interaction,
 
 @tree.command(name="start-round",
               description="Creates and enables the current round, if possible",
-              guild=discord.Object(id=947928220093788180))
+              guild=discord.Object(id=GUILD_ID))
 @app_commands.default_permissions(manage_guild=True)
 async def start_round(interaction: discord.Interaction):
     log_interaction_call(interaction)
@@ -170,9 +189,9 @@ async def start_round(interaction: discord.Interaction):
 
 @tree.command(name="end-round",
               description="Ends the current round, if possible",
-              guild=discord.Object(id=947928220093788180))
+              guild=discord.Object(id=GUILD_ID))
 @app_commands.default_permissions(manage_guild=True)
-async def start_round(interaction: discord.Interaction):
+async def end_round(interaction: discord.Interaction):
     log_interaction_call(interaction)
     game = await get_game(BASE_PATH)
 
@@ -182,14 +201,14 @@ async def start_round(interaction: discord.Interaction):
         await interaction.response.send_message(f'There is not currently an active round to end!', ephemeral=True)
         return
     else:
-        latest_round.is_active = False
+        latest_round.is_active_round = False
 
     await write_game(game, BASE_PATH)
     await interaction.response.send_message(f'Ended round {latest_round.round_number}!', ephemeral=True)
 
 @tree.command(name="incarcerate-player",
               description="Toggles a player status of being incarcerated or not.",
-              guild=discord.Object(id=947928220093788180))
+              guild=discord.Object(id=GUILD_ID))
 @app_commands.default_permissions(manage_guild=True)
 async def incarcerate_player(interaction: discord.Interaction,
                      player: discord.Member,
@@ -209,7 +228,7 @@ async def incarcerate_player(interaction: discord.Interaction,
 
 @tree.command(name="kill-player",
               description="Toggles a player status of being dead or not.",
-              guild=discord.Object(id=947928220093788180))
+              guild=discord.Object(id=GUILD_ID))
 @app_commands.default_permissions(manage_guild=True)
 async def kill_player(interaction: discord.Interaction,
                      player: discord.Member,
@@ -230,7 +249,7 @@ async def kill_player(interaction: discord.Interaction,
 
 @tree.command(name="refresh-withdrawals",
               description="Toggles a player status of being dead or not.",
-              guild=discord.Object(id=947928220093788180))
+              guild=discord.Object(id=GUILD_ID))
 @app_commands.default_permissions(manage_guild=True)
 async def refresh_withdrawals(interaction: discord.Interaction):
     log_interaction_call(interaction)
@@ -246,7 +265,7 @@ async def refresh_withdrawals(interaction: discord.Interaction):
 
 @tree.command(name="deposit",
               description="Deposit assets from your personal stash to your faction's holdings",
-              guild=discord.Object(id=947928220093788180))
+              guild=discord.Object(id=GUILD_ID))
 @app_commands.checks.cooldown(1, 5, key=lambda i: i.guild_id)
 async def deposit(interaction: discord.Interaction,
                   amount: app_commands.Range[int, 0, 20]):
@@ -285,7 +304,7 @@ async def deposit(interaction: discord.Interaction,
 
 @tree.command(name="withdraw",
               description="Withdraw assets from your faction's holdings into your personal stash",
-              guild=discord.Object(id=947928220093788180))
+              guild=discord.Object(id=GUILD_ID))
 @app_commands.checks.cooldown(1, 5, key=lambda i: i.guild_id)
 async def withdraw(interaction: discord.Interaction,
                    amount: app_commands.Range[int, 0, 20]):
@@ -326,7 +345,7 @@ async def withdraw(interaction: discord.Interaction,
 
 @tree.command(name="transfer",
               description="Transfer assets from your personal stash to another player",
-              guild=discord.Object(id=947928220093788180))
+              guild=discord.Object(id=GUILD_ID))
 @app_commands.checks.cooldown(1, 5, key=lambda i: i.guild_id)
 async def transfer(interaction: discord.Interaction,
                    player: discord.Member,
@@ -372,7 +391,7 @@ async def transfer(interaction: discord.Interaction,
 
 @tree.command(name="balance",
               description="Get the current balance of assets in your personal stash, or your faction's holdings",
-              guild=discord.Object(id=947928220093788180))
+              guild=discord.Object(id=GUILD_ID))
 @app_commands.checks.cooldown(1, 5, key=lambda i: i.guild_id)
 async def balance(interaction: discord.Interaction,
                   of_type: Literal['Player', 'Faction', 'Tension']):
@@ -410,9 +429,20 @@ async def balance(interaction: discord.Interaction,
         else:
             await interaction.response.send_message(f'Current faction holdings for Faction {requested_faction.faction_name} is {requested_faction.assets}', ephemeral=True)
 
+@tree.command(name="role-player-test",
+              description="Select only a specific player from a list",
+              guild=discord.Object(id=GUILD_ID))
+@app_commands.autocomplete(player=player_list_autocomplete)
+async def role_player_test(interaction: discord.Interaction,
+                           player: str):
+    log_interaction_call(interaction)
+
+    await interaction.response.send_message(f'Got player {player}!', ephemeral=True)
+
+
 @tree.command(name="vote-player",
               description="Votes for a particular player",
-              guild=discord.Object(id=947928220093788180))
+              guild=discord.Object(id=GUILD_ID))
 @app_commands.checks.cooldown(1, 5, key=lambda i: i.guild_id)
 async def vote_player(interaction: discord.Interaction,
                       player: Optional[discord.Member] = None,
@@ -425,7 +455,7 @@ async def vote_player(interaction: discord.Interaction,
         return
 
     latest_round = game.get_latest_round()
-    if latest_round is None:
+    if latest_round is None or not latest_round.is_active_round:
         await interaction.response.send_message(f'No currently active round found for this game!', ephemeral=True)
         return
 
@@ -472,7 +502,7 @@ async def vote_player(interaction: discord.Interaction,
 
 @tree.command(name="vote-report",
               description="Generates a report of current voting totals",
-              guild=discord.Object(id=947928220093788180))
+              guild=discord.Object(id=GUILD_ID))
 @app_commands.checks.cooldown(1, 5, key=lambda i: i.guild_id)
 async def vote_report(interaction: discord.Interaction,
                       for_round: Optional[app_commands.Range[int, 0, 20]] = None,
@@ -501,7 +531,7 @@ async def vote_report(interaction: discord.Interaction,
         else:
             vote_dict[vote.choice] = [vote.player_id]
 
-    formatted_votes = f"**Vote Totals as of <t:{int(time.time())}>**\n"
+    formatted_votes = f"**Vote Totals for round {report_round.round_number} as of <t:{int(time.time())}>**\n"
     formatted_votes += "```\n"
     for key, value in sorted(vote_dict.items(), key=lambda e: len(e[1]), reverse=True):
         if key == 'No Vote':
@@ -544,6 +574,5 @@ async def write_game(game: Game, path: str):
 def log_interaction_call(interaction: discord.Interaction):
     logger.info(
         f'Received command {interaction.command.name} with parameters {interaction.data} initiated by user {interaction.user.name}')
-
 
 client.run(TOKEN)
